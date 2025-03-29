e5961433105d38898d62c0caf5591b0255a55dc5
crj23ne8 <cristyjose8991@gmail.com>
2025-03-29 11:25:29 +0100

    Solved errors in Bounded and added one shape

--- components/Bounded.tsx
+++ components/Bounded.tsx
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { forwardRef, ElementType } from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-    as?: React.ElementType;
-    className?: string;
-    children: React.ReactNode;
+type BoundedProps<T extends ElementType = "section"> = {
+  as?: T;
+  className?: string;
+  children?: React.ReactNode;
 };
 
-const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
-    ({ as: Comp = "section", className, children, ...restProps }, ref) => {
-        return (
-            <Comp 
-            ref={ref} 
-            className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-            {...restProps}>
-            <div className="mx-auto w-full max-w-7xl">
-                {children}
-            </div>
-            </Comp>
-        );
-    }
+const Bounded = forwardRef<HTMLDivElement, BoundedProps>(
+  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
+      </Comp>
+    );
+  }
 );
 
 Bounded.displayName = "Bounded";
 
-export default Bounded
+export default Bounded;
--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,11 +1,12 @@
 "use client"; // This is a client component
 
-import { FC } from "react";
 import { useEffect, useRef } from "react";
+import React from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap, { random } from "gsap";
 import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 
 /**
  * Props for `Hero`.
@@ -15,89 +16,95 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 /**
  * Component for "Hero" Slices.
  */
-const Hero: FC<HeroProps> = ({ slice }) => {
-  const component = useRef(null)
+const Hero = ({ slice }: HeroProps): React.JSX.Element => {
+  const component = useRef<HTMLDivElement>(null);
 
-  useEffect(()=>{
-    let ctx = gsap.context(()=>{
-      const t1 = gsap.timeline()
-      
-        t1.fromTo(".name-animation",{
-          x: -100, opacity:0, rotate: -10
-        },
-      {
-        x:0,
-        opacity: 1,
-        rotate:0,
-        ease: "elastic.out(1,0.3)",
-        duration: 1,
-        transformOrigin: "left top",
-        delay: 0.5,
-        stagger: { // makes the letters come out at different times 
-          each: 0.1, 
-          from: "random",
-        }
-      }
-    );
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
 
       t1.fromTo(
-        ".job-title", {
-          y:20,
+        ".name-animation",
+        {
+          x: -100,
           opacity: 0,
-          scale:1.2,
-        }, {
-          opacity:1,
-          y:0,
+          rotate: -10,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3)",
+          duration: 1,
+          transformOrigin: "left top",
+          delay: 0.5,
+          stagger: {
+            // makes the letters come out at different times
+            each: 0.1,
+            from: "random",
+          },
+        }
+      );
+
+      t1.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          opacity: 1,
+          y: 0,
           duration: 1,
           scale: 1,
           ease: "elastic.out(1,0.3)",
         }
-      )
-
-
-    }, component)
-    return () => ctx.revert(); // animation cleanup  
-    }, []) // empty means it only runs once
-
+      );
+    }, component);
+    return () => ctx.revert(); // animation cleanup
+  }, []); // empty means it only runs once
 
   const renderLetters = (name: KeyTextField, key: string) => {
     if (!name) return;
-    return name.split("").map((letter,index) => (
-      <span 
-      key={index} 
-      className={`name-animation name-animation-${key} inline-block opacity-0`}>
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+      >
         {letter}
       </span>
     ));
-  }
+  };
 
-  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      ref = {component}
+      ref={component}
     >
-      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center" >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
-          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" 
-          aria-label={
-            slice.primary.name + " " + slice.primary.second_name
-            }>
-
-           <span className="block text-slate-300">
-            {renderLetters(slice.primary.name, "first")}
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            aria-label={slice.primary.name + " " + slice.primary.second_name}
+          >
+            <span className="block text-slate-300">
+              {renderLetters(slice.primary.name, "first")}
             </span>
 
-           <span className="-mt-[.2em] block text-slate-500">
-            {renderLetters(slice.primary.second_name, "last")}
+            <span className="-mt-[.2em] block text-slate-500">
+              {renderLetters(slice.primary.second_name, "last")}
             </span>
-            </h1>
-           <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text 
+          </h1>
+          <span
+            className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text 
            text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 
-           md:text-4xl">
+           md:text-4xl"
+          >
             {slice.primary.tagline}
-            </span>
+          </span>
         </div>
       </div>
     </Bounded>
